807d745e1f15ef97b10adc3b315ce825661e884d
Bozhidar-Yovchev <bozhidar601@gmail.com>
2024-09-17 16:46:24 +0300

    Lenght of last word

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Length_of_Last_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int count = 0;
+            bool countingStarted = false;
+
+            for (int i = s.Length-1; i >= 0; i--)
+            {
+                if (s[i] != ' ')
+                {
+                    count++;
+                    countingStarted = true;
+                }
+                else if (!countingStarted)
+                {
+                    count = 0;
+                }
+                else 
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
